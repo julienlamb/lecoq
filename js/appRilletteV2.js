735f13bca057e68bab5e504930b20df599ecd3d9
julienlamb <julienlambert29@gmail.com>
2023-06-24 18:08:42 +0200

    Ajout image pintage, trio rillettes. js appRillette

---
+++ js/appRilletteV2.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+	//Gestion du texte des tailles de poulets
+	$('#btnPiment').on('click', function (e) {
+		$("#titreTaille").html('Rillettes piment d&#39;espelette');
+		$("#prixRillette").html('3,50€');
+		$("#qteRillette").html(' les 90g');
+		$("#presentationIngredients").html('<b class="font-weight-bold">Ingrédients :</b> Viande et gras de poulet origine France (99%), sel de Guérande, poivre, piment d&#39;espelette.');
+		$('#imgRillettes').attr('src','../assets/img/logo/rillette_piment.png');
+	});
+	$('#btnNature').on('click', function (e) {
+		$("#titreTaille").html('Rillettes nature');
+		$("#prixRillette").html('5,50€');
+		$("#qteRillette").html(' les 180g');
+		$("#presentationIngredients").html('<b class="font-weight-bold">Ingrédients :</b> Viande et gras de poulet origine France (99%), sel de Guérande, poivre.');
+		$('#imgRillettes').attr('src','../assets/img/logo/rillettes.png');
+	});
+	$('#btnCitron').on('click', function (e) {
+		$("#titreTaille").html('Rillettes citron & thyn');
+		$("#prixRillette").html('3,50€');
+		$("#qteRillette").html(' les 90g');
+		$("#presentationIngredients").html('<b class="font-weight-bold">Ingrédients :</b> Viande et gras de poulet origine France (99%), sel de Guérande, poivre, citron & thyn.');
+		$('#imgRillettes').attr('src','../assets/img/logo/rillette_citron.png');
+	});
+});
