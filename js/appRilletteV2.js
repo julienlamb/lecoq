076e13ddaa1311f04affaa436ec0a2aa8110ac87
julienlamb <julienlambert29@gmail.com>
2023-06-29 22:37:18 +0200

    le '&' ne conctionne pas=> correction

--- js/appRilletteV2.js
+++ js/appRilletteV2.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
 		$('#imgRillettes').attr('src','../assets/img/logo/rillettes.png');
 	});
 	$('#btnCitron').on('click', function (e) {
-		$("#titreTaille").html('Rillettes citron & thyn');
+		$("#titreTaille").html('Rillettes citron et thyn');
 		$("#prixRillette").html('3,50€');
 		$("#qteRillette").html(' les 90g');
-		$("#presentationIngredients").html('<b class="font-weight-bold">Ingrédients :</b> Viande et gras de poulet origine France (99%), sel de Guérande, poivre, citron & thyn.');
+		$("#presentationIngredients").html('<b class="font-weight-bold">Ingrédients :</b> Viande et gras de poulet origine France (99%), sel de Guérande, poivre, citron et thyn.');
 		$('#imgRillettes').attr('src','../assets/img/logo/rillette_citron.png');
 	});
 });
